4141e413be790049fa8c711da93cc41c9fdc5da7
littlefox698 <littlefox698@proton.me>
2025-11-06 21:10:53 +0800

    Update upload-agsb-v2.py

--- upload-agsb-v2.py
+++ upload-agsb-v2.py
@@ -31,11 +31,11 @@ DEBUG_LOG = INSTALL_DIR / "python_debug.log"
 CUSTOM_DOMAIN_FILE = INSTALL_DIR / "custom_domain.txt" # 存储最终使用的域名
 
 # ====== 全局可配置参数（可直接在此处修改） ======
-USER_NAME = "kkddytdlala"         # 用户名
-UUID = "a91b59b6-ade4-497d-b4e9-88d184c48048"                     # UUID，留空则自动生成
-PORT = 49999                   # Vmess端口，留空或0则自动生成
-DOMAIN = "free.testlrj.ddns-ip.net"                   # 域名，留空则自动获取
-CF_TOKEN = "eyJhIjoiODBmMjY5ZmQ1N2QzNzNiMmMzZTBkODc4ODg1NWM5MzIiLCJ0IjoiZmVhMzBmODUtOGY5OC00ZTVmLTkyZTktMmU2OTk2M2E1YzUyIiwicyI6Ik4yTmlZemxpTlRjdE5UVm1PQzAwTjJZekxXRmpORGt0TVdVNE5HUmtORGN3TldObSJ9"                 # Cloudflare Token，留空则用Quick Tunnel
+USER_NAME = "kkxx"         # 用户名
+UUID = "3e5c4df4-3c5d-42e9-9353-cb1c5c7a5f1e"                     # UUID，留空则自动生成
+PORT = 1888                   # Vmess端口，留空或0则自动生成
+DOMAIN = "k.longdick.qzz.io"                   # 域名，留空则自动获取
+CF_TOKEN = "eyJhIjoiYTQ4NTE5NDhiNDcxYmViYWUyOGVhMTY4MDgxOTE1ZGMiLCJ0IjoiYTQ2NjdmODItYzc5NS00OTk2LWIwYjQtMDg0OTYzNzc2Yzk4IiwicyI6Ik1UUTBNbU5qWmpFdE1qVmhOUzAwWXpFMExUZ3dNbUl0TVRRMk1EYzRZVGxpTVRKbCJ9"                 # Cloudflare Token，留空则用Quick Tunnel
 # =========================================
 
 # 添加命令行参数解析
